1283c8f6b4a6154297769b090a39d5611d4c10a8
munkimu <naduh@list.ru>
2023-05-02 00:18:29 +0300

    много чего

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+
+let navAll = document.querySelector('.navigation');
+
+let navListList = navAll.querySelector('.navigation__list')
+let navToggle = navAll.querySelector('.navigation__toggle');
+
+navAll.classList.remove('navigation--no-js');
+
+navToggle.onclick = function() {
+  navToggle.classList.toggle('navigation__toggle--is-open');
+  navListList.classList.toggle('navigation__list--visible');
+}
